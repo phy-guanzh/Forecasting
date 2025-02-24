ca2e928b4c4156592c6d767425507c1dd0cc7205
Zhe Guan <zhe.guan@cern.ch>
2025-02-24 11:19:07 +0000

    update

--- Basic_forecasting /NaiveForecasting.py
+++ Basic_forecasting /NaiveForecasting.py
@@ -3,6 +3,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.graphics.tsaplots import plot_acf
 
 # Load the dataset
 data = pd.read_excel("../Describe/BuildingMaterials.xls", index_col=0, parse_dates= True, date_format="%Y %b", header=None, names=["Date", "Amount"] )[:-4]
@@ -36,11 +37,42 @@ plt.plot(data_NF1["Amount"], label="Naive Forecast NF1", color="blue")
 plt.plot(data_NF2["Amount"], label="Seasonal Naive Forecast NF2",color="red")
 plt.legend()
 plt.title("Building Materials Sales Naive Forecasting")
-
+plt.savefig("Naive_Forecast.png")
 
 plt.show()
 
+#ME values
+data["Diff_NF1"] = data["Amount"] - data_NF1["Amount"]
+data["Diff_NF2"] = data["Amount"] - data_NF2["Amount"]
 
+error_keys = {
+    "Mean Error": [
+        data["Diff_NF1"].mean(),
+        data["Diff_NF2"].mean()
+    ],
+    "Mean Absolute Error": [
+        data["Diff_NF1"].abs().sum()/data["Diff_NF1"].count(),
+        data["Diff_NF2"].abs().sum()/data["Diff_NF2"].count()
+    ],
+    "Mean Squared Error": [
+        ((data["Diff_NF1"] ** 2).sum())/data["Diff_NF1"].count(),
+        (data["Diff_NF2"] ** 2).sum()/data["Diff_NF2"].count()
+    ],
+    "Mean Absolute Percentage Error": [
+        (data["Diff_NF1"].abs().sum()/data["Diff_NF1"].count())*100,
+        (data["Diff_NF2"].abs().sum()/data["Diff_NF2"].count())*100
+    ]
+}
+print(error_keys)
 
+data_NF1["Amount"] = data_NF1["Amount"].fillna(0)
+data_NF2["Amount"] = data_NF2["Amount"].fillna(method="ffill")
 
-print(addictive)
+fig, ax = plt.subplots(nrows=3, ncols=1,figsize= (12, 10))
+plot_acf(data["Amount"], ax= ax[0], title="Auto-correlation with lag12", lags=12)
+plot_acf(data_NF1["Amount"], ax= ax[1], title = "Auto-cor NF1")
+plot_acf(data_NF2["Amount"], ax= ax[2], title = "Auto-cor NF2")
+
+print(data_NF1["Amount"])
+plt.savefig("ACF.png")
+plt.show()
